8488c7d8ab7182ac819cef4132cbac773e641b75
Stéphanie Xavier <stephanie.exd1997@gmail.com>
2025-06-10 19:43:11 -0300

    Update InvoiceServices.java

--- src/main/java/com/accounting/invoice_manager/application/service/InvoiceServices.java
+++ src/main/java/com/accounting/invoice_manager/application/service/InvoiceServices.java
@@ -5,17 +5,24 @@ import com.accounting.invoice_manager.infrastructure.repository.InvoiceRepositor
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class InvoiceServices {
-  private final InvoiceRepository invoiceRepository;
+    private final InvoiceRepository invoiceRepository;
 
     public InvoiceServices(InvoiceRepository invoiceRepository) {
         this.invoiceRepository = invoiceRepository;
     }
 
-    public List<InvoiceModel> findByUserId(UUID id){
-        return invoiceRepository.findByUserId(id);
+    public List<InvoiceModel> findByUserId(UUID userId){
+        return invoiceRepository.findByUserId(userId);
+    }
+
+    public Optional<InvoiceModel> findById(UUID id){
+        return invoiceRepository.findById(id);
     }
 }
+
+
